d4292fb6a4a2697f862f9e2c14e2a17fd16b32d6
Rolf Erik lekang <me@rolflekang.com>
2014-09-20 10:32:30 +0200

    update python dependency script

--- python/install.sh
+++ python/install.sh
@@ -1,1 +1,27 @@
-easy_install SpoofMAC
+#!/bin/sh
+
+echo "Installing Python dependencies"
+install_python_dependency() {
+  if test ! $(which $2)
+  then
+    echo "  Installing ${1} for you."
+    pip install $1 > /tmp/$1-install.log
+  else
+    echo "  ${1} already installed."
+  fi
+}
+
+if test ! $(which pip)
+then
+  echo "  Installing pip for you."
+  wget https://bootstrap.pypa.io/get-pip.py > /tmp/pip-install.log
+  python get-pip.py >> /tmp/pip-install.log
+else
+  echo "  pip already installed."
+fi
+
+install_python_dependency virtualenv virtualenv
+install_python_dependency virtualenvwrapper virtualenvwrapper.sh
+install_python_dependency flake8 flake8
+install_python_dependency tox tox
+install_python_dependency fabric fab
